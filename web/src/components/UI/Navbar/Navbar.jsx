503fd866f96fac20463aa56a84c1e445b77d5a74
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-09-13 22:29:25 +0300

    Navbar fix

--- web/src/components/UI/Navbar/Navbar.jsx
+++ web/src/components/UI/Navbar/Navbar.jsx
@@ -2,27 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import cls from './Navbar.module.scss';
 export default function Navbar() {
+  const LINKS = [
+    ['/', 'Главная страница'],
+    ['/theory', 'Теоретические сведения'],
+    ['/structure', 'Структура установки'],
+    ['/simulator', 'Симулятор сведения'],
+    ['/contacts', 'Контакты'],
+  ];
   return (
     <div className={cls.navbar}>
       <nav>
         <ul className={cls.list}>
-          <li className={cls.list_item}>
-            <Link to="/">Главная страница</Link>
-          </li>
-          <li className={cls.list_item}>
-            <Link to="/theory">Теоретические сведения</Link>
-          </li>
-          <li className={cls.list_item}>
-            <Link to="/structure">Структура установки</Link>
-          </li>
-          <li className={cls.list_item}>
-            <Link to="/simulator">Симулятор установки</Link>
-          </li>
-          <li className={cls.list_item}>
-            <div className={cls.contacts_btn}>
-              <Link to="/contacts">Контакты</Link>
-            </div>
-          </li>
+          {LINKS.map((link) => (
+            <li className={cls.list_item} key={link[0] + link[1]}>
+              <Link to={link[0]}>{link[1]}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
