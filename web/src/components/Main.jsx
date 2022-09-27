be35e237fbeefae8f72171e67cfd241b9175e9d8
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-09-27 22:43:43 +0300

    Structure Page base

--- web/src/components/Main.jsx
+++ web/src/components/Main.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ContactsPage from '../pages/ContactsPage/ContactsPage.jsx';
 import MainPage from '../pages/MainPage/MainPage.jsx';
+import StructurePage from '../pages/StructurePage/StructurePage.jsx';
 import TheoryPage from '../pages/TheoryPage/TheoryPage.jsx';
 export default function Main() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/theory" element={<TheoryPage />} />
-      <Route path="/structure" element={<h1>3</h1>} />
+      <Route path="/structure" element={<StructurePage />} />
       <Route path="/simulator" element={<h1>4</h1>} />
       <Route path="/contacts" element={<ContactsPage />} />
     </Routes>
---
+++ web/src/pages/StructurePage/StructurePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from '../../components/UI/Image/Image.jsx';
+import Installation from '../../img/Installation.jpg';
+import ss from './StructurePage.module.scss';
+export default function StructurePage() {
+  return (
+    <div>
+      <h1>Структура и описание установки </h1>
+      <Image src={Installation} classNameImg={ss.img} />
+    </div>
+  );
+}
