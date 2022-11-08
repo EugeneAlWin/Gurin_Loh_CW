c0ed4729c880ad6644239df95df056efd1651cbc
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-11-08 21:49:11 +0300

    project arch cleaned

---
+++ sim/Assets/CameraScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class CameraScript: MonoBehaviour
+{
+    [SerializeField]
+    float scrollSpeed = 10f;
+    [SerializeField]
+    int sensivity = 3;
+    [SerializeField]
+    Transform targetPos;
+    int maxdistance = 20;
+    int mindistance = 1;
+
+    //  ФУНКЦИЯ ОГРАНИЧЕНИЯ ПРЕДЕЛОВ ДВИЖЕНИЯ КАМЕРЫ
+    bool ControlDistance(float distance)
+    {
+        if (distance > mindistance && distance < maxdistance) return true;
+        return false;
+    }
+
+    // ВРАЩЕНИЕ ВОКРУГ ЦЕНТРАЛЬНОЙ ТОЧКИ УСТАНОВКИ С ЗАЖАТОЙ ПРАВОЙ КЛАВИШЕЙ МЫШИ
+    void Update()
+    {
+        if (Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        {
+            transform.RotateAround(targetPos.position, Vector3.up, Input.GetAxis("Mouse X") * sensivity);
+        }
+        if (Input.GetMouseButton(2) && !Input.GetMouseButton(1))
+        {
+            transform.RotateAround(targetPos.position, Vector3.right, Input.GetAxis("Mouse Y") * sensivity);
+        }
+        // ДВИЖЕНИЯ КАМЕРЫ В СТОРОНЫ КЛАВИШАМИ
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        if (x != 0 || y != 0)
+        {
+            Vector3 newpos = transform.position + (transform.TransformDirection(new Vector3(x, 0, 0)) + Vector3.forward * y) / sensivity;
+            if (ControlDistance(Vector3.Distance(newpos, targetPos.position))) transform.position = newpos;
+        }
+
+        // ПРИБЛИЖЕНИЕ И УДАЛЕНИЕ КАМЕРЫ ОТ УСТАНОВКИ ПРОКРУТКОЙ КОЛЕСА МЫШИ
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            Vector3 newpos = transform.position + transform.TransformDirection(Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Vector3.up );
+            transform.position = newpos;
+            if (transform.position.y < 13f) transform.position = new Vector3(transform.position.x, 13f, transform.position.z);
+            if (transform.position.y > 16f) transform.position = new Vector3(transform.position.x, 16f, transform.position.z);
+        }
+
+    }
+}
