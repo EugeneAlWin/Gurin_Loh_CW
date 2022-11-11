9c594dfeb1743c4ef863746ead740822e6bb60eb
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-11-11 10:53:48 +0300

    practice buttons and exit button added

---
+++ sim/Assets/ButtonsCanvasScripts/ExitButton.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)=>Application.Quit();
+}
