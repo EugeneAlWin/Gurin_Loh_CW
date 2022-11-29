64bee5eee892a5726c68b49a9013bb2faaf7d56b
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-11-30 02:24:53 +0300

    the table pops up when hover over the button

---
+++ sim/Assets/CANVAS/CanvasScripts/GroundPanel/ShowTableOnHover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowTableOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    internal GameObject table;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        table.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        table.SetActive(false);
+    }
+
+    void Start()
+    {
+        table = GameObject.Find("Table");
+    }
+}
