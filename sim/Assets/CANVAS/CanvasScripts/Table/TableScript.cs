d8d42c1de9a96e82cece04a2299c8611473ec85a
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-12-06 21:27:15 +0300

    fix cells search in table (T and R)

--- sim/Assets/CANVAS/CanvasScripts/Table/TableScript.cs
+++ sim/Assets/CANVAS/CanvasScripts/Table/TableScript.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class TableScript : MonoBehaviour
 {
+    [SerializeField] private int resAndTempCount;
     internal GameObject table;
     private GameObject[] tableCellsT, TableCellsR;
     internal Text[] temps, resists;
@@ -12,8 +13,14 @@ public class TableScript : MonoBehaviour
         showTableOnHover = GameObject.Find("ShowTable").GetComponent<ShowTableOnHover>();
         table = GameObject.Find("Table");
         showTableOnHover.table = table;
-        tableCellsT = GameObject.FindGameObjectsWithTag("TEMPERATURE");
-        TableCellsR = GameObject.FindGameObjectsWithTag("RESISTANCE");
+        tableCellsT = new GameObject[9];
+        TableCellsR = new GameObject[9];
+        for (byte i = 1; i <= resAndTempCount; i++)
+        {
+            tableCellsT[i-1] = GameObject.Find("T" + i);
+            TableCellsR[i-1] = GameObject.Find("R" + i);
+        }
+
         temps = new Text[tableCellsT.Length];
         resists = new Text[TableCellsR.Length];
         for (int i = 0; i < tableCellsT.Length; i++)
